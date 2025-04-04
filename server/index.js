f31b6d72109ad77ec9da676b8d14e4b694603407
Phani Charan <charanphani5@gmail.com>
2025-04-04 13:56:23 -0500

    Revert Group D Changes

--- server/index.js
+++ server/index.js
@@ -81,7 +81,6 @@ app.post("/api/createUser", async (req, res) => {
   }
 });
 
-HEAD
 // Graceful shutdown
 process.on("SIGINT", () => {
   mongoose.connection.close(() => {
@@ -90,5 +89,5 @@ process.on("SIGINT", () => {
   });
 });
 
-const PORT = process.env.PORT || 5004;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
