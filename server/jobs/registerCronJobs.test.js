be1bb1e5b76bf35013cd50025e5a1240b3add9ab
Phani Charan <charanphani5@gmail.com>
2025-04-08 09:27:29 -0500

    Update Tests

--- server/jobs/registerCronJobs.test.js
+++ server/jobs/registerCronJobs.test.js
@@ -4,20 +4,21 @@ const coordinatorReminder = require("./reminderEmail");
 jest.mock("../utils/cronUtils");
 
 describe("registerCronJobs", () => {
-    beforeEach(() => {
-        cronJobManager.registerJob.mockClear();
-    });
-    
-    it("registers coordinator cron job", () => {
-        require("./registerCronJobs");
-        // Check registerJob was called
-        expect(cronJobManager.registerJob).toHaveBeenCalledTimes(1);
-        expect(cronJobManager.registerJob).toHaveBeenCalledWith("coordinatorApprovalReminder",
-            "*/2 * * * *",
-            coordinatorReminder,
-            {
-                runOnInit: true,
-                timezone: "Asia/Kolkata",
-        });
-    });
-})
+  beforeEach(() => {
+    cronJobManager.registerJob.mockClear();
+  });
+
+  it("registers coordinator cron job", () => {
+    require("./registerCronJobs");
+    // Check registerJob was called
+    expect(cronJobManager.registerJob).toHaveBeenCalledTimes(1);
+    expect(cronJobManager.registerJob).toHaveBeenCalledWith(
+      "coordinatorApprovalReminder",
+      "*/2 * * * *",
+      coordinatorReminder,
+      {
+        timezone: "Asia/Kolkata",
+      }
+    );
+  });
+});
