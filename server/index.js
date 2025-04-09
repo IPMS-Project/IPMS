e7673c74f231987c07707376bedf410e37592c99
sanjay347 <kotturi.sanjay2003@gmail.com>
2025-04-09 00:19:32 -0500

    Updated with changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const User = require("./models/User");
+const formRoutes = require("./routes/formRoutes");
+
 require("dotenv").config();
 
 const emailRoutes = require("./routes/emailRoutes");
